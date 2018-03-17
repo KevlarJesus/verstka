c7bcb0c68a94185c5a21c67c11646fd4559e67e3
KevlarJesus <masfel13@gmail.com>
2018-03-18 02:35:50 +0500

    разораблся с галпжс

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,13 @@
 var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    cssnext = require('postcss-cssnext');
 
 gulp.task('css', function () {
-  return (
-    gulp.src('app/*.css')
-    .pipe(postcss([
-      //require("postcss-import")(),
-      //require("postcss-url")(),
-      require("postcss-cssnext")()
-      // add your "plugins" here
-      // ...
-      // and if you want to compress
-      // Disable autoprefixer, because it's already included in cssnext
-      // require("cssnano")({ autoprefixer: false }),
-      //require("postcss-browser-reporter")(),
-      //require("postcss-reporter")(),
-      //http://cssnext.io/postcss/
-      //https://www.npmjs.com/package/gulp-postcss
-    ]))
-    .pipe(gulp.dest('dest/css'))
-  )
-})
+  return gulp.src('app/css/*.css')
+    .pipe(postcss([cssnext()]))
+    .pipe(gulp.dest('dest/css'));
+});
 
 gulp.task('browser-sync', function() {
     browserSync({
