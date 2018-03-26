3d3e7ace2c9111e17b097fca01ce18de9db04efd
Ilya Katuntsev <masfel13@gmail.com>
2018-03-26 17:48:04 +0500

    оптимизация изображений

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     browserSync = require('browser-sync'),
-    cssnext = require('postcss-cssnext');
+    cssnext = require('postcss-cssnext'),
+    imageop = require('gulp-image-optimization'),
+    svgo = require('gulp-svgo');
 
 gulp.task('css', function () {
   return gulp.src('app/css/*.css')
@@ -23,4 +25,20 @@ gulp.task('watch', ['browser-sync', 'css'], function() {
     gulp.watch('app/css/*.css', ['css']);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/*.js', browserSync.reload);
+});
+
+gulp.task('images', function(cb) {
+    gulp.src(['app/img/*.png','app/img/*.jpg','app/img/*.gif','app/img/*.jpeg']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    }))
+    .pipe(gulp.dest('dist/img'))
+    .on('end', cb).on('error', cb);
+});
+
+gulp.task('svg', () => {
+    return gulp.src('app/img/*')
+        .pipe(svgo())
+        .pipe(gulp.dest('dist/img'));
 });
